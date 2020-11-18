4e75465f3d9ead223ce2b0b308c09e1049fa0e4c
노보람 <boramm@mindslab.ai>
2020-11-19 06:09:45 +0900

    no message

--- project_web/resources/js/common.js
+++ project_web/resources/js/common.js
@@ -1,1 +1,20 @@
 /* MINDsLab. NBR. 20201118 */
+
+jQuery.event.add(window,"load",function(){
+    $(document).ready(function(){
+        // 공통 레이어 팝업
+        $('.btn_lyr_open').on('click',function(){
+            var popHref = $(this).attr('href');
+
+            // popup open
+            $('body').addClass('lyr_bg_dim');
+            $(popHref).css({'display': 'block'});
+
+            // popup close
+            $('.btn_lyr_close, .btn_lyr_save').on('click',function(){
+                $('body').removeClass('lyr_bg_dim');
+                $('.lyrBox').css({'display': 'none'});
+            });
+        });
+    });
+});
