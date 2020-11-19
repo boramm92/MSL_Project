972becc25637ecc4b1597a816b7d8aaf2f3da912
노보람 <boramm@mindslab.ai>
2020-11-20 01:11:07 +0900

    no message

--- project_web/resources/js/common.js
+++ project_web/resources/js/common.js
@@ -2,23 +2,38 @@
 
 jQuery.event.add(window,"load",function(){
     $(document).ready(function(){
+        // // 공통 레이어 팝업
+        // $('.btn_lyr_open').on('click',function(){
+        //     var popHref = $(this).attr('href');
+        //
+        //     // popup open
+        //     $('body').addClass('lyr_bg_dim');
+        //     $(popHref).css({'display': 'block'});
+        //
+        //     // popup close
+        //     $('.btn_lyr_close, .btn_lyr_save').on('click',function(){
+        //         $('body').removeClass('lyr_bg_dim');
+        //         $('.lyrBox').css({'display': 'none'});
+        //     });
+        // });
+
         // 공통 레이어 팝업
         $('.btn_lyr_open').on('click',function(){
             var popHref = $(this).attr('href');
 
             // popup open
-            $('body').addClass('lyr_bg_dim');
+            $('body').css({'overflow': 'hidden'});
             $(popHref).css({'display': 'block'});
 
             // popup close
             $('.btn_lyr_close, .btn_lyr_save').on('click',function(){
-                $('body').removeClass('lyr_bg_dim');
-                $('.lyrBox').css({'display': 'none'});
+                $('body').css({'overflow': 'visible'});
+                $('.lyr_wrap').css({'display': 'none'});
             });
         });
-        
+
         // 인풋 라벨
-        var placeholderLabel = $('input[type="text"]');
+        var placeholderLabel = $('.iptText');
 
         placeholderLabel.on('focus', function(){
             $(this).siblings('label').hide();
