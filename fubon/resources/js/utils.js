d77adf82341f4df5609daf57ee3294b7ab422a4f
노보람 <boramm@mindslab.ai>
2020-11-25 19:57:00 +0900

    no message

--- fubon/resources/js/utils.js
+++ fubon/resources/js/utils.js
@@ -1,187 +1,147 @@
-import {spritingOn, spritingOff} from './spriting';
+'use strict';
 
-function createCanvas(w, h) {
-	var canvas = document.createElement('canvas');
-	canvas.width = w;
-	canvas.height = h;
-	return canvas;
-}
-
-function readImageData(url, callback) {
-	var image = new Image();
-
-	image.onload = function() {
-		var h = image.height;
-		var w = image.width;
-		var canvas = createCanvas(w, h);
-		var ctx = canvas.getContext('2d');
-		ctx.drawImage(image, 0, 0, w, h);
-		callback(ctx.getImageData(0, 0, w, h));
-	};
-
-	image.src = url;
-}
-
-/**
- * Injects a new canvas (and div wrapper) and creates the associated Chart instance
- * using the given config. Additional options allow tweaking elements generation.
- * @param {object} config - Chart config.
- * @param {object} options - Chart acquisition options.
- * @param {object} options.canvas - Canvas attributes.
- * @param {object} options.wrapper - Canvas wrapper attributes.
- * @param {boolean} options.useOffscreenCanvas - use an OffscreenCanvas instead of the normal HTMLCanvasElement.
- * @param {boolean} options.useShadowDOM - use shadowDom
- * @param {boolean} options.persistent - If true, the chart will not be released after the spec.
- */
-function acquireChart(config, options) {
-	var wrapper = document.createElement('div');
-	var canvas = document.createElement('canvas');
-	var chart, key;
-
-	config = config || {};
-	options = options || {};
-	options.canvas = options.canvas || {height: 512, width: 512};
-	options.wrapper = options.wrapper || {class: 'chartjs-wrapper'};
-
-	for (key in options.canvas) {
-		if (Object.prototype.hasOwnProperty.call(options.canvas, key)) {
-			canvas.setAttribute(key, options.canvas[key]);
-		}
-	}
-
-	for (key in options.wrapper) {
-		if (Object.prototype.hasOwnProperty.call(options.wrapper, key)) {
-			wrapper.setAttribute(key, options.wrapper[key]);
-		}
-	}
-
-	// by default, remove chart animation and auto resize
-	config.options = config.options || {};
-	config.options.animation = config.options.animation === undefined ? false : config.options.animation;
-	config.options.responsive = config.options.responsive === undefined ? false : config.options.responsive;
-	config.options.locale = config.options.locale || 'en-US';
-
-	if (options.useShadowDOM) {
-		if (!wrapper.attachShadow) {
-			// If shadowDOM is not supported by the browsers, mark test as 'pending'.
-			return pending();
-		}
-		wrapper.attachShadow({mode: 'open'}).appendChild(canvas);
-	} else {
-		wrapper.appendChild(canvas);
-	}
-	window.document.body.appendChild(wrapper);
-
-	try {
-		var ctx;
-		if (options.useOffscreenCanvas) {
-			if (!canvas.transferControlToOffscreen) {
-				// If this browser does not support offscreen canvas, mark the test as 'pending', which will skip the
-				// test.
-				// TODO: switch to skip() once it's implemented (https://github.com/jasmine/jasmine/issues/1709), or
-				// remove if all browsers implement `transferControlToOffscreen`
-				return pending();
-			}
-			var offscreenCanvas = canvas.transferControlToOffscreen();
-			ctx = offscreenCanvas.getContext('2d');
-		} else {
-			ctx = canvas.getContext('2d');
-		}
-		if (options.spriteText) {
-			spritingOn(ctx);
-		}
-		chart = new Chart(ctx, config);
-	} catch (e) {
-		window.document.body.removeChild(wrapper);
-		throw e;
-	}
-
-	chart.$test = {
-		persistent: options.persistent,
-		wrapper: wrapper
-	};
-
-	return chart;
-}
-
-function releaseChart(chart) {
-	spritingOff(chart.ctx);
-	chart.destroy();
-
-	var wrapper = (chart.$test || {}).wrapper;
-	if (wrapper && wrapper.parentNode) {
-		wrapper.parentNode.removeChild(wrapper);
-	}
-}
-
-function injectCSS(css) {
-	// https://stackoverflow.com/q/3922139
-	var head = document.getElementsByTagName('head')[0];
-	var style = document.createElement('style');
-	style.setAttribute('type', 'text/css');
-	if (style.styleSheet) { // IE
-		style.styleSheet.cssText = css;
-	} else {
-		style.appendChild(document.createTextNode(css));
-	}
-	head.appendChild(style);
-}
-
-function waitForResize(chart, callback) {
-	var override = chart.resize;
-	chart.resize = function() {
-		chart.resize = override;
-		override.apply(this, arguments);
-		callback();
-	};
-}
-
-function afterEvent(chart, type, callback) {
-	var override = chart._eventHandler;
-	chart._eventHandler = function(event) {
-		override.call(this, event);
-		if (event.type === type) {
-			chart._eventHandler = override;
-			// eslint-disable-next-line callback-return
-			callback();
-		}
-	};
-}
-
-function _resolveElementPoint(el) {
-	var point = {x: 0, y: 0};
-	if (el) {
-		if (typeof el.getCenterPoint === 'function') {
-			point = el.getCenterPoint();
-		} else if (el.x !== undefined && el.y !== undefined) {
-			point = el;
-		}
-	}
-	return point;
-}
-
-function triggerMouseEvent(chart, type, el) {
-	var node = chart.canvas;
-	var rect = node.getBoundingClientRect();
-	var point = _resolveElementPoint(el);
-	var event = new MouseEvent(type, {
-		clientX: rect.left + point.x,
-		clientY: rect.top + point.y,
-		cancelable: true,
-		bubbles: true,
-		view: window
-	});
-
-	node.dispatchEvent(event);
-}
-
-export default {
-	injectCSS: injectCSS,
-	createCanvas: createCanvas,
-	acquireChart: acquireChart,
-	releaseChart: releaseChart,
-	readImageData: readImageData,
-	triggerMouseEvent: triggerMouseEvent,
-	waitForResize: waitForResize,
-	afterEvent: afterEvent
+window.chartColors = {
+	red: 'rgb(211, 0, 0)',
+	orange: 'rgb(255, 154, 29)',
+	yellow: 'rgb(255, 205, 86)',
+	green: 'rgb(75, 192, 192)',
+	blue: 'rgb(50, 139, 216)',
+	purple: 'rgb(153, 102, 255)',
+	grey: 'rgb(201, 203, 207)'
 };
+
+(function(global) {
+	var MONTHS = [
+		'January',
+		'February',
+		'March',
+		'April',
+		'May',
+		'June',
+		'July',
+		'August',
+		'September',
+		'October',
+		'November',
+		'December'
+	];
+
+	var COLORS = [
+		'#4dc9f6',
+		'#f67019',
+		'#f53794',
+		'#537bc4',
+		'#acc236',
+		'#166a8f',
+		'#00a950',
+		'#58595b',
+		'#8549ba'
+	];
+
+	var Samples = global.Samples || (global.Samples = {});
+	var Color = global.Color;
+
+	Samples.utils = {
+		// Adapted from http://indiegamr.com/generate-repeatable-random-numbers-in-js/
+		srand: function(seed) {
+			this._seed = seed;
+		},
+
+		rand: function(min, max) {
+			var seed = this._seed;
+			min = min === undefined ? 0 : min;
+			max = max === undefined ? 1 : max;
+			this._seed = (seed * 9301 + 49297) % 233280;
+			return min + (this._seed / 233280) * (max - min);
+		},
+
+		numbers: function(config) {
+			var cfg = config || {};
+			var min = cfg.min || 0;
+			var max = cfg.max || 1;
+			var from = cfg.from || [];
+			var count = cfg.count || 8;
+			var decimals = cfg.decimals || 8;
+			var continuity = cfg.continuity || 1;
+			var dfactor = Math.pow(10, decimals) || 0;
+			var data = [];
+			var i, value;
+
+			for (i = 0; i < count; ++i) {
+				value = (from[i] || 0) + this.rand(min, max);
+				if (this.rand() <= continuity) {
+					data.push(Math.round(dfactor * value) / dfactor);
+				} else {
+					data.push(null);
+				}
+			}
+
+			return data;
+		},
+
+		labels: function(config) {
+			var cfg = config || {};
+			var min = cfg.min || 0;
+			var max = cfg.max || 100;
+			var count = cfg.count || 8;
+			var step = (max - min) / count;
+			var decimals = cfg.decimals || 8;
+			var dfactor = Math.pow(10, decimals) || 0;
+			var prefix = cfg.prefix || '';
+			var values = [];
+			var i;
+
+			for (i = min; i < max; i += step) {
+				values.push(prefix + Math.round(dfactor * i) / dfactor);
+			}
+
+			return values;
+		},
+
+		months: function(config) {
+			var cfg = config || {};
+			var count = cfg.count || 12;
+			var section = cfg.section;
+			var values = [];
+			var i, value;
+
+			for (i = 0; i < count; ++i) {
+				value = MONTHS[Math.ceil(i) % 12];
+				values.push(value.substring(0, section));
+			}
+
+			return values;
+		},
+
+		color: function(index) {
+			return COLORS[index % COLORS.length];
+		},
+
+		transparentize: function(color, opacity) {
+			var alpha = opacity === undefined ? 0.5 : 1 - opacity;
+			return Color(color).alpha(alpha).rgbString();
+		}
+	};
+
+	// DEPRECATED
+	window.randomScalingFactor = function() {
+		return Math.round(Samples.utils.rand(-100, 100));
+	};
+
+	// INITIALIZATION
+
+	Samples.utils.srand(Date.now());
+
+	// Google Analytics
+	/* eslint-disable */
+	if (document.location.hostname.match(/^(www\.)?chartjs\.org$/)) {
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+		ga('create', 'UA-28909194-3', 'auto');
+		ga('send', 'pageview');
+	}
+	/* eslint-enable */
+
+}(this));
