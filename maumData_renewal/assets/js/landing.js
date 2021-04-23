5c6b2401c286fd78dbcfeb797f0569a70c042d84
노보람 <boramm@mindslab.ai>
2021-04-23 17:41:56 +0900

    [추가] 마음데이터 - 랜딩페이지 html, css 추가

--- maumData_renewal/assets/js/landing.js
+++ maumData_renewal/assets/js/landing.js
@@ -1,6 +1,6 @@
 /* MINDsLab. NBR. 20210422 */
 
-// header fixed, background change
+// header 스크롤 효과
 const header = document.querySelector('#header');
 
 function headerScrollFunc(){ 
@@ -11,3 +11,28 @@ function headerScrollFunc(){
     } 
 } 
 window.addEventListener('scroll', headerScrollFunc);
+
+// stn - use_case 탭기능
+let tabBtn = $('.tabArea .tab_lst > li');
+let tabCont = $('.tabArea .tab_cont > li');
+
+tabBtn.on('click', function(){
+    var target = $(this);
+    var index = target.index();
+    tabBtn.removeClass('active');
+    target.addClass('active');
+    tabCont.removeClass('active');
+    tabCont.eq(index).addClass('active');
+});
+
+let subBtn = $('.sub_lst li');
+let subCont = $('.sub_cont li');
+
+subBtn.on('click', function(){
+    var target = $(this);
+    var index = target.index();
+    subBtn.removeClass('active');font
+    target.addClass('active');
+    subCont.removeClass('active');
+    subCont.eq(index).addClass('active');
+});
