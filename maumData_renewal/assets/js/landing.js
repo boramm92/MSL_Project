45c2a0dac7db578da3f271b20ddc237133c53a25
노보람 <boramm316@gmail.com>
2021-04-26 01:04:05 +0900

    [수정] maum Data - 랜딩페이지 수정

--- maumData_renewal/assets/js/landing.js
+++ maumData_renewal/assets/js/landing.js
@@ -1,38 +1,162 @@
 /* MINDsLab. NBR. 20210422 */
 
 // header 스크롤 효과
-const header = document.querySelector('#header');
+function headerScrollFunc(e){ 
+    var header = document.getElementById('header');
 
-function headerScrollFunc(){ 
     if (pageYOffset > 5){ 
         header.classList.add('fixed'); 
     }else{ 
         header.classList.remove('fixed'); 
     } 
+    
+    // var target = e.target;
+    // console.log(target);
+    // var scrollX = window.scrollLeft;
+    // header.style.left = scrollX + 'px';
+    // console.log(scrollX);
 } 
+window.addEventListener('load', headerScrollFunc);
 window.addEventListener('scroll', headerScrollFunc);
 
-// stn - use_case 탭기능
-let tabBtn = $('.tabArea .tab_lst > li');
-let tabCont = $('.tabArea .tab_cont > li');
+// window.addEventListener('scroll', function(){
+//     var header = document.querySelector('#header');
+//     var scrollX = window.scrollLeft;
+//     header.style.left = '500px';
+//     console.log(scrollX)
+// });
 
-tabBtn.on('click', function(){
-    var target = $(this);
-    var index = target.index();
-    tabBtn.removeClass('active');
-    target.addClass('active');
-    tabCont.removeClass('active');
-    tabCont.eq(index).addClass('active');
+// header 좌우 스크롤 처리
+$(window).scroll(function(){
+    var scrollX = $(window).scrollLeft()
+    $('#header').css('left', - scrollX);
+})
+
+// header 언어선택 (웹, 모바일 별 기능)
+var langSelectWrap = document.querySelector('.lang_slt_wrap'),
+    langSelectBox = document.querySelector('.slt_box'),
+    langSelectBtn = document.getElementsByClassName('btn_lang_slt');
+
+function webLangSeleceFunc(){
+    langSelectBox.addEventListener('click', function(){
+        langSelectWrap.classList.add('open');
+        for(var i = 0; i < langSelectBtn.length; i++){
+            langSelectBtn[i].classList.add('on');
+            langSelectBtn[i].addEventListener('click', function(e){
+                var target = e.target;
+                langSelectWrap.classList.remove('open');
+                if(target.classList.contains('lang_ko')){
+                    document.querySelector('.lang_en').classList.remove('on');
+                }else{
+                    document.querySelector('.lang_ko').classList.remove('on');
+                }
+            });
+        }    
+    });
+}
+function mobileLangSeleceFunc(){
+    function btnClick(i){
+        langSelectBtn[i].addEventListener('click', function(){
+            langSelectBtn[i].classList.add('on');
+            if(langSelectBtn[i].classList.contains('lang_ko')){
+                document.querySelector('.lang_en').classList.remove('on');
+            }else{
+                document.querySelector('.lang_ko').classList.remove('on');
+            }
+        });
+    }
+    for(var i = 0; i < langSelectBtn.length; i++){
+        btnClick(i);  
+    }   
+}
+window.addEventListener('load', function(){
+    if(window.innerWidth <= 768){
+        mobileLangSeleceFunc();
+    }else{
+        webLangSeleceFunc();
+    }
+});
+window.addEventListener('resize', function(){
+    if(window.innerWidth <= 768){
+        mobileLangSeleceFunc();
+    }else{
+        webLangSeleceFunc();
+    }
+});   
+
+// header 모바일 메뉴 햄버거 버튼
+// var hamBtn = document.querySelector('.btn_ham'),
+//     sta = document.querySelector('.sta'),
+//     bgDim = document.querySelector('.bg_dim'),
+//     headerCont = document.getElementById('header').childNodes[1],
+//     clicked = false;
+
+// hamBtn.addEventListener('click', function(){
+//     if(!clicked){
+//         hamBtn.classList.add('active');
+//         sta.classList.add('active');
+//         // document.getElementsByTagName('body')[0].style.overflow = 'hidden';
+//         var bgDimCreat = document.createElement('div');
+//         bgDimCreat.innerHTML = '<div class="bg_dim"></div>';
+//         headerCont.insertAdjacentText('beforeend', bgDimCreat);
+//         clicked = true;
+
+//         bgDim.addEventListener('click', function(){
+//             hamBtn.classList.remove('active');
+//             sta.classList.remove('active');
+//             document.getElementsByTagName('body')[0].style.overflow = '';
+//             bgDim.remove();
+//             clicked = false;
+//         });
+//     }else{
+//         hamBtn.classList.remove('active');
+//         sta.classList.remove('active');
+//         document.getElementsByTagName('body')[0].style.overflow = '';
+//         bgDim.remove();
+//         clicked = false;
+//     }
+// });
+
+var clicked = false;
+
+$('.btn_ham').click(function(){
+    if(!clicked){
+        $(this).addClass('active');
+        $('.sta').addClass('active');
+        $('body').css('overflow', 'hidden');
+        $('#header .contBox').append('<div class="bg_dim"></div>');
+        clicked = true;
+        $('.bg_dim').on('click', function(){
+            $('.btn_ham').removeClass('active');
+            $('.sta').removeClass('active');
+            $('body').css('overflow', '');
+            $(this).remove();
+            clicked = false;
+        });       
+    }else{
+        $(this).removeClass('active');
+        $('.sta').removeClass('active');
+        $('body').css('overflow', '');
+        $('.bg_dim').remove();
+        clicked = false;
+    }
 });
 
-let subBtn = $('.sub_lst li');
-let subCont = $('.sub_cont li');
+// 모바일 gnb nav li 순서 변경
+// function gnbNavOrderChange(){
+//     var navDataVoucher = document.querySelector('.dataVoucher'),
+//         parent = navDataVoucher.parentNode;
 
-subBtn.on('click', function(){
-    var target = $(this);
-    var index = target.index();
-    subBtn.removeClass('active');font
-    target.addClass('active');
-    subCont.removeClass('active');
-    subCont.eq(index).addClass('active');
-});
+//     if(window.innerWidth <= 768){
+//         parent.insertBefore(navDataVoucher, parent.childNodes[2]);
+//     }else{
+//         parent.insertBefore(navDataVoucher, parent.lastChild);
+//     }
+// }
+// window.addEventListener('load', gnbNavOrderChange);
+// window.addEventListener('resize', gnbNavOrderChange);
+
+
+
+
+
