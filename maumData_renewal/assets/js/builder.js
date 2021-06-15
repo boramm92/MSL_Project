7363b906db540c66a2b3f0bcfbaa80c2465da103
노보람 <boramm@mindslab.ai>
2021-06-15 18:09:00 +0900

    [renewal tool] 이미지 작업화면 html, css, js 수정

--- maumData_renewal/assets/js/builder.js
+++ maumData_renewal/assets/js/builder.js
@@ -1,10 +1,5 @@
 /* MINDsLab. NBR. 20210604 */
 $(document).ready(function(){
-    // header scrollX func
-    // $(window).scroll(function () {
-	// 	$('#header').css('left', 0 - $(this).scrollLeft());
-	// });
-
     // tool menu active func
     var toolMenuBtn = $('.menu_list li button');
     var classBox = $('.menu_list li .classBox');
@@ -31,4 +26,35 @@ $(document).ready(function(){
     $('.btn_expand').on('click', function(){
         $('.aside').toggleClass('on');
     });    
+
+    // comments slide toggle & tab height 
+    $('.comments .tit').on('click', function(){
+        $('.cmt_box').slideToggle(200);
+    });
+
+    // aside tab
+    var asideTabBtn = $('.tabWrap .tab_btn > button');
+    var asideTabCont = $('.tabWrap .tab_cont > ul');
+
+    asideTabCont.eq(0).show();
+    asideTabBtn.on('click',function(){
+        var index = $(this).index();
+        asideTabBtn.removeClass('active');
+        $(this).addClass('active');
+        asideTabCont.css('display','none');
+        asideTabCont.eq(index).css('display','block');
+    });
+
+    // 
+    $('.tabWrap .tab_cont > ul li').on('click', function(){
+        if($(this).is('.active')){
+            $(this).removeClass('active');
+        }else{
+            $('.tabWrap .tab_cont > ul li').removeClass('active');
+            $(this).addClass('active');
+        }
+    });
+    $('.tabWrap .tab_cont > ul li .select').on('click', function(e){
+        e.stopPropagation();
+    });
 });
