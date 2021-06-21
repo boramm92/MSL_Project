491213a9cd5e81c6f4d73f889d1d9886f2e8bf64
노보람 <boramm316@gmail.com>
2021-06-22 02:02:41 +0900

    no message

--- maumData_renewal/assets/js/builder.js
+++ maumData_renewal/assets/js/builder.js
@@ -1,108 +1,5 @@
 /* MINDsLab. NBR. 20210604 */
 $(document).ready(function(){
-    // tool menu 활성화
-    var toolMenuBtn = $('.menu_list li button');
-    var classBox = $('.menu_list li .classBox');
-
-    toolMenuBtn.on('click', function(){
-        var thisClassBox = $(this).next('.classBox');
-
-        if($(this).hasClass('active')){
-            $(this).removeClass('active open');
-            thisClassBox.removeClass('active');
-        }else{
-            toolMenuBtn.removeClass('active');
-            classBox.removeClass('active');
-            thisClassBox.addClass('active'); 
-            if($(this).next('.classBox').length){
-                $(this).addClass('active open'); 
-            }else{
-                $(this).addClass('active'); 
-            }           
-        }
-    });
-
-    // aside 영역 확장 버튼
-    $('.btn_expand').on('click', function(){
-        $('.aside').toggleClass('on');
-    });    
-
-    // comments 클릭 시 박스 슬라이드 & height 조절
-    var clicked = false; 
-
-    $('.comments .tit').on('click', function(){
-        var asideHeight = $('.aside').outerHeight(),
-            currentFileHeight = $('.aside .currentFile').outerHeight(),
-            guideFileHeight = $('.aside .guideFile').outerHeight(), 
-            commentsHeight = $('.aside .comments').outerHeight(), 
-            commentBoxHeight = $('.aside .comments .cmt_box').outerHeight(),            
-            deleteBoxHeight = $('.aside .deleteBox').outerHeight();
-
-        if(!clicked){
-            var height = currentFileHeight + guideFileHeight + commentsHeight + deleteBoxHeight + commentBoxHeight;
-
-            $('.comments .cmt_box').slideDown(200);
-            $('.aside .tabWrap').animate({height: asideHeight - height}, 200);
-            clicked = true;
-        }else{
-            var height = currentFileHeight + guideFileHeight + commentsHeight + deleteBoxHeight - commentBoxHeight;
-
-            $('.comments .cmt_box').slideUp(200);  
-            $('.aside .tabWrap').animate({height: asideHeight - height}, 200);         
-            clicked = false;
-        }
-    });
-
-    // resize 시 박스 슬라이드 & height 조절
-    $(window).resize(function(){
-        var asideHeight = $('.aside').outerHeight(),
-            currentFileHeight = $('.aside .currentFile').outerHeight(),
-            guideFileHeight = $('.aside .guideFile').outerHeight(), 
-            commentsHeight = $('.aside .comments').outerHeight(),            
-            deleteBoxHeight = $('.aside .deleteBox').outerHeight();
-
-            var height = currentFileHeight + guideFileHeight + commentsHeight + deleteBoxHeight;
-            $('.aside .tabWrap').css({height: asideHeight - height});
-    });
-
-    // aside tab
-    var asideTabBtn = $('.tabWrap .tab_btn > button');
-    var asideTabCont = $('.tabWrap .tab_cont > ul');
-
-    asideTabCont.eq(0).show();
-    asideTabBtn.on('click',function(){
-        var index = $(this).index();
-        asideTabBtn.removeClass('active');
-        $(this).addClass('active');
-        asideTabCont.css('display','none');
-        asideTabCont.eq(index).css('display','block');
-    });
-
-    // 태그 리스트 
-    $('.tag_list li.reject').clone().appendTo('.reject_list');  // 반려건만 클론해서 반려목록에 넣기
-
-    $('.tabWrap .tab_cont > ul li .tagBox').on('click', function(){
-        var thisParentList = $(this).parent('li');
-
-        if(thisParentList.is('.active')){
-            thisParentList.removeClass('active');
-
-            if(thisParentList.find('.reject_txt').length){
-                thisParentList.find('.reject_txt').slideToggle(200);
-            }
-        }else{
-            $('.tabWrap .tab_cont > ul li').removeClass('active');
-            thisParentList.addClass('active');
-
-            if(thisParentList.find('.reject_txt').length){
-                thisParentList.find('.reject_txt').slideToggle(200);
-            }
-        }
-    });
-    $('.tabWrap .tab_cont > ul li .select').on('click', function(e){
-        e.stopPropagation();
-    });
-
     // 공통 layer popup 
 	$('.btn_lyr_open').on('click',function(){	
         var btnLyrName = $(this).data('lyr-name'),
@@ -148,5 +45,113 @@ $(document).ready(function(){
             $('.history_list li a').removeClass('active');
             $(this).addClass('active');
         });
+    });
+
+    // tool menu 활성화
+    var toolMenuBtn = $('.menu_list li button');
+    var classBox = $('.menu_list li .classBox');
+
+    toolMenuBtn.on('click', function(){
+        var thisClassBox = $(this).next('.classBox');
+
+        if($(this).hasClass('active')){
+            $(this).removeClass('active open');
+            thisClassBox.removeClass('active');
+        }else{
+            toolMenuBtn.removeClass('active');
+            classBox.removeClass('active');
+            thisClassBox.addClass('active'); 
+            if($(this).next('.classBox').length){
+                $(this).addClass('active open'); 
+            }else{
+                $(this).addClass('active'); 
+            }           
+        }
+    });
+
+    // aside 영역 확장 버튼
+    $('.btn_expand').on('click', function(){
+        $('.aside').toggleClass('on');
+    });    
+
+    // comments 클릭 시 박스 슬라이드 & height 조절
+    $('.comments .tit').on('click', function(){
+        var asideHeight = $('.aside').outerHeight(),
+            currentFileHeight = $('.aside .currentFile').outerHeight(),
+            guideFileHeight = $('.aside .guideFile').outerHeight(), 
+            commentsHeight = $('.aside .comments').outerHeight(), 
+            commentBoxHeight = $('.aside .comments .cmt_box').outerHeight(),            
+            deleteBoxHeight = $('.aside .deleteBox').outerHeight();
+
+        if(commentsHeight < 55){
+            var height = currentFileHeight + guideFileHeight + commentsHeight + deleteBoxHeight + commentBoxHeight;
+
+            $('.comments .cmt_box').slideDown(200);
+            $('.aside .tabWrap').animate({height: asideHeight - height}, 200);
+        }else{
+            var height = currentFileHeight + guideFileHeight + commentsHeight + deleteBoxHeight - commentBoxHeight;
+
+            $('.comments .cmt_box').slideUp(200);  
+            $('.aside .tabWrap').animate({height: asideHeight - height}, 200);         
+        }
+    });
+
+    // resize 시 박스 슬라이드 & height 조절
+    function handleTabWrapHeight(){
+        var asideHeight = $('.aside').outerHeight(),
+            currentFileHeight = $('.aside .currentFile').outerHeight(),
+            guideFileHeight = $('.aside .guideFile').outerHeight(), 
+            commentsHeight = $('.aside .comments').outerHeight(),            
+            deleteBoxHeight = $('.aside .deleteBox').outerHeight();
+
+        var height = currentFileHeight + guideFileHeight + commentsHeight + deleteBoxHeight;
+        $('.aside .tabWrap').css({height: asideHeight - height});
+    } 
+    handleTabWrapHeight();  
+
+    $(window).resize(function(){
+        handleTabWrapHeight();
+    });
+
+    // aside tab
+    var asideTabBtn = $('.tabWrap .tab_btn > button');
+    var asideTabCont = $('.tabWrap .tab_cont > ul');
+
+    asideTabCont.eq(0).show();
+    asideTabBtn.on('click',function(){
+        var index = $(this).index();
+        asideTabBtn.removeClass('active');
+        $(this).addClass('active');
+        asideTabCont.css('display','none');
+        asideTabCont.eq(index).css('display','block');
+    });
+
+    // 태그 리스트 
+    $('.tag_list li.reject').clone().appendTo('.reject_list');  // 반려건만 클론해서 반려목록에 넣기
+
+    $('.tabWrap .tab_cont > ul li .tagBox').on('click', function(){
+        var thisParentList = $(this).parent('li');
+
+        if(thisParentList.is('.active')){
+            thisParentList.removeClass('active');
+
+            if(thisParentList.find('.reject_txt').length){
+                thisParentList.find('.reject_txt').slideToggle(200);
+            }else if(thisParentList.find('.editCommentBox').length){
+                thisParentList.find('.editCommentBox').slideToggle(200);
+            }
+        }else{
+            $('.tabWrap .tab_cont > ul li').removeClass('active');
+            thisParentList.addClass('active');
+
+            if(thisParentList.find('.reject_txt').length){
+                thisParentList.find('.reject_txt').slideToggle(200);
+            }else if(thisParentList.find('.editCommentBox').length){
+                thisParentList.find('.editCommentBox').slideToggle(200);
+            }
+        }
+    });
+    $('.tabWrap .tab_cont > ul li .select').on('click', function(e){
+        e.stopPropagation();
     });
 });
