2328536354f797dce078219be6eacfeea4b34180
노보람 <boramm316@gmail.com>
2021-08-06 03:57:38 +0900

    no message

--- maumEvent/resources/js/scroll.js
+++ maumEvent/resources/js/scroll.js
@@ -1,36 +1,89 @@
 $(document).ready(function(){
-    var allIndexItem = document.querySelectorAll('.index_item');
-    var allStickyItem = document.querySelectorAll('.scroll_item');
-    var currentStickyItem = allStickyItem[0];
-
-    for(var i = 0; i < allIndexItem.length; i++){
-        allIndexItem[i].dataset.index = i;
-        allStickyItem[i].dataset.index = i;
-    }
-
-    function activate(){
-        currentStickyItem.classList.add('visible');
-    }
-
-    function inactivate(){
-        currentStickyItem.classList.remove('visible');
-    }
-
-    window.addEventListener('scroll', function(){
-        var stickyItem;
-        var boundingRect;
+    function stickyEffect(){
+        var allIndexItem = document.querySelectorAll('.index_item');
+        var allStickyItem = document.querySelectorAll('.scroll_item');
+        var currentStickyItem = allStickyItem[0];
 
         for(var i = 0; i < allIndexItem.length; i++){
-            stickyItem = allIndexItem[i];
-            boundingRect = stickyItem.getBoundingClientRect();
-
-            if(boundingRect.top > window.innerHeight * 0.1 && boundingRect.top < window.innerHeight * 0.8){
-                inactivate();
-                currentStickyItem = allStickyItem[stickyItem.dataset.index];
-                activate();
-            }
+            allIndexItem[i].dataset.index = i;
+            allStickyItem[i].dataset.index = i;
         }
-    });
 
-    activate();
+        function activate(){
+            currentStickyItem.classList.add('visible');
+        }
+
+        function inactivate(){
+            currentStickyItem.classList.remove('visible');
+        }
+
+        window.addEventListener('scroll', function(){
+            var stickyItem;
+            var boundingRect;
+
+            for(var i = 0; i < allIndexItem.length; i++){
+                stickyItem = allIndexItem[i];
+                boundingRect = stickyItem.getBoundingClientRect();
+
+                if(boundingRect.top > window.innerHeight * 0.4 && boundingRect.top < window.innerHeight * 0.6){
+                    inactivate();
+                    currentStickyItem = allStickyItem[stickyItem.dataset.index];
+                    activate();
+                }
+            }
+        });
+
+        activate();
+    }
+
+    if($(window).outerWidth() > 768){
+        stickyEffect();
+    }else{
+        return false;
+    }
 });
+
+$(window).resize(function(){
+    function stickyEffect(){
+        var allIndexItem = document.querySelectorAll('.index_item');
+        var allStickyItem = document.querySelectorAll('.scroll_item');
+        var currentStickyItem = allStickyItem[0];
+
+        for(var i = 0; i < allIndexItem.length; i++){
+            allIndexItem[i].dataset.index = i;
+            allStickyItem[i].dataset.index = i;
+        }
+
+        function activate(){
+            currentStickyItem.classList.add('visible');
+        }
+
+        function inactivate(){
+            currentStickyItem.classList.remove('visible');
+        }
+
+        window.addEventListener('scroll', function(){
+            var stickyItem;
+            var boundingRect;
+
+            for(var i = 0; i < allIndexItem.length; i++){
+                stickyItem = allIndexItem[i];
+                boundingRect = stickyItem.getBoundingClientRect();
+
+                if(boundingRect.top > window.innerHeight * 0.4 && boundingRect.top < window.innerHeight * 0.6){
+                    inactivate();
+                    currentStickyItem = allStickyItem[stickyItem.dataset.index];
+                    activate();
+                }
+            }
+        });
+
+        activate();
+    }
+
+    if($(window).outerWidth() > 768){
+        stickyEffect();
+    }else{
+        return false;
+    }
+})
