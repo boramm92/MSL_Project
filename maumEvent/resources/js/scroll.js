ca081362c2f438c443605e0f40ac4dac4e3978dc
노보람 <boramm316@gmail.com>
2021-08-06 01:20:09 +0900

    no message

--- maumEvent/resources/js/scroll.js
+++ maumEvent/resources/js/scroll.js
@@ -1,49 +1,36 @@
 $(document).ready(function(){
-    // var allIndexItem = document.querySelectorAll('.index_item');
-    // var allStickyItem = document.querySelectorAll('.scroll_item');
-    // var currentStickyItem = allStickyItem[0];
+    var allIndexItem = document.querySelectorAll('.index_item');
+    var allStickyItem = document.querySelectorAll('.scroll_item');
+    var currentStickyItem = allStickyItem[0];
 
-    // for(var i = 0; i < allIndexItem.length; i++){
-    //     allIndexItem[i].dataset.index = i;
-    //     allStickyItem[i].dataset.index = i;
-    // }
-
-    // function activate(){
-    //     currentStickyItem.classList.add('visible');
-    // }
-
-    // function inactivate(){
-    //     currentStickyItem.classList.remove('visible');
-    // }
-
-    // window.addEventListener('scroll', function(){
-    //     var stickyItem;
-    //     var boundingRect;
-
-    //     for(var i = 0; i < allIndexItem.length; i++){
-    //         stickyItem = allIndexItem[i];
-    //         boundingRect = stickyItem.getBoundingClientRect();
-
-    //         if(boundingRect.top > window.innerHeight * 0.1 && boundingRect.top < window.innerHeight * 0.8){
-    //             inactivate();
-    //             currentStickyItem = allStickyItem[stickyItem.dataset.index];
-    //             activate();
-    //         }
-    //     }
-    // });
-
-    // activate();
-
-
-    function introStnShape(){
-        var width = $(window).outerWidth();
-        var height = $(window).outerHeight();
-
-        $('.main .oblique_line').css('border-right-width', width);
+    for(var i = 0; i < allIndexItem.length; i++){
+        allIndexItem[i].dataset.index = i;
+        allStickyItem[i].dataset.index = i;
     }
-    introStnShape();
-});
 
-$(window).resize(function(){
-    introStnShape();
+    function activate(){
+        currentStickyItem.classList.add('visible');
+    }
+
+    function inactivate(){
+        currentStickyItem.classList.remove('visible');
+    }
+
+    window.addEventListener('scroll', function(){
+        var stickyItem;
+        var boundingRect;
+
+        for(var i = 0; i < allIndexItem.length; i++){
+            stickyItem = allIndexItem[i];
+            boundingRect = stickyItem.getBoundingClientRect();
+
+            if(boundingRect.top > window.innerHeight * 0.1 && boundingRect.top < window.innerHeight * 0.8){
+                inactivate();
+                currentStickyItem = allStickyItem[stickyItem.dataset.index];
+                activate();
+            }
+        }
+    });
+
+    activate();
 });
