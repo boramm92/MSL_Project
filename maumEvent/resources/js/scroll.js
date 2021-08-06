ffb661682314fde18d31b8feadca19f3ba9e7b09
노보람 <boramm@mindslab.ai>
2021-08-06 18:13:33 +0900

    no message

--- maumEvent/resources/js/scroll.js
+++ maumEvent/resources/js/scroll.js
@@ -1,89 +1,66 @@
 $(document).ready(function(){
-    function stickyEffect(){
-        var allIndexItem = document.querySelectorAll('.index_item');
-        var allStickyItem = document.querySelectorAll('.scroll_item');
-        var currentStickyItem = allStickyItem[0];
+    var allIndexItem = document.querySelectorAll('.index_item');
+    var allStickyItem = document.querySelectorAll('.scroll_item');
+    var currentStickyItem = allStickyItem[0];
+
+    for(var i = 0; i < allIndexItem.length; i++){
+        allIndexItem[i].dataset.index = i;
+        allStickyItem[i].dataset.index = i;
+    }
+
+    function activate(){       
+        currentStickyItem.classList.add('visible');       
+    }
+
+    function inactivate(){
+        currentStickyItem.classList.remove('visible');        
+    }
+
+    function fixed(){
+        currentStickyItem.classList.remove('static');
+        currentStickyItem.classList.add('fixed');
+    }
+
+    function static(){
+        currentStickyItem.classList.remove('fixed');
+        currentStickyItem.classList.add('static');
+    }
+
+    window.addEventListener('scroll', function(){
+        var stickyItem;
+        var boundingRect;
 
         for(var i = 0; i < allIndexItem.length; i++){
-            allIndexItem[i].dataset.index = i;
-            allStickyItem[i].dataset.index = i;
-        }
+            stickyItem = allIndexItem[i];
+            boundingRect = stickyItem.getBoundingClientRect();
 
-        function activate(){
-            currentStickyItem.classList.add('visible');
-        }
-
-        function inactivate(){
-            currentStickyItem.classList.remove('visible');
-        }
-
-        window.addEventListener('scroll', function(){
-            var stickyItem;
-            var boundingRect;
-
-            for(var i = 0; i < allIndexItem.length; i++){
-                stickyItem = allIndexItem[i];
-                boundingRect = stickyItem.getBoundingClientRect();
-
-                if(boundingRect.top > window.innerHeight * 0.4 && boundingRect.top < window.innerHeight * 0.6){
-                    inactivate();
-                    currentStickyItem = allStickyItem[stickyItem.dataset.index];
-                    activate();
-                }
+            if(boundingRect.top > window.innerHeight * 0.4 && boundingRect.top < window.innerHeight * 0.6){
+                inactivate();
+                static();
+                currentStickyItem = allStickyItem[stickyItem.dataset.index];
+                activate();
+                fixed();
             }
-        });
+        }
+    });
+    activate();
 
-        activate();
-    }
 
-    if($(window).outerWidth() > 768){
-        stickyEffect();
-    }else{
-        return false;
-    }
+    // function animationSpeech() {
+    //     $('.speechBox').removeClass('active');
+    //     setTimeout(function(){
+    //         $('.speechBox').addClass('active');
+    //     }, 100);
+    // }
+
+    // if($(window).outerWidth() <= 768){
+    //     setInterval(animationSpeech, 6100);
+    // }
+
+    // $(window).resize(function(){
+    //     if($(window).outerWidth() <= 768){
+    //         setInterval(animationSpeech, 6100);
+    //     }
+    // });
 });
 
-$(window).resize(function(){
-    function stickyEffect(){
-        var allIndexItem = document.querySelectorAll('.index_item');
-        var allStickyItem = document.querySelectorAll('.scroll_item');
-        var currentStickyItem = allStickyItem[0];
-
-        for(var i = 0; i < allIndexItem.length; i++){
-            allIndexItem[i].dataset.index = i;
-            allStickyItem[i].dataset.index = i;
-        }
-
-        function activate(){
-            currentStickyItem.classList.add('visible');
-        }
-
-        function inactivate(){
-            currentStickyItem.classList.remove('visible');
-        }
-
-        window.addEventListener('scroll', function(){
-            var stickyItem;
-            var boundingRect;
-
-            for(var i = 0; i < allIndexItem.length; i++){
-                stickyItem = allIndexItem[i];
-                boundingRect = stickyItem.getBoundingClientRect();
-
-                if(boundingRect.top > window.innerHeight * 0.4 && boundingRect.top < window.innerHeight * 0.6){
-                    inactivate();
-                    currentStickyItem = allStickyItem[stickyItem.dataset.index];
-                    activate();
-                }
-            }
-        });
-
-        activate();
-    }
-
-    if($(window).outerWidth() > 768){
-        stickyEffect();
-    }else{
-        return false;
-    }
-})
