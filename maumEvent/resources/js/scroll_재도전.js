f30bf0b0ef901c8b73921715e50a8390e99c6766
노보람 <boramm@mindslab.ai>
2021-08-09 16:52:51 +0900

    no message

--- maumEvent/resources/js/scroll_재도전.js
+++ maumEvent/resources/js/scroll_재도전.js
@@ -23,15 +23,32 @@ $(document).ready(function(){
         for(var i = 0; i < allIndexItem.length; i++){
             stickyItem = allIndexItem[i];
             boundingRect = stickyItem.getBoundingClientRect();
+            scrollAreaBoundingRect = scrollArea.getBoundingClientRect();
 
             if(boundingRect.top > window.innerHeight * 0.1 && boundingRect.top < window.innerHeight * 0.9){
                 inactivate();
                 currentStickyItem = allStickyItem[stickyItem.dataset.index];
                 activate();  
-            }  
+            }
+
+            // console.log(window.pageYOffset, window.pageYOffset + scrollAreaBoundingRect.top);
+
+            // console.log(currentStickyItem.getBoundingClientRect().top)
+            
+            // if(window.pageYOffset >= window.pageYOffset + scrollAreaBoundingRect.top){
+            //     scrollArea.classList.add('fixed');                
+            // }
+            // if(window.pageYOffset + currentStickyItem.getBoundingClientRect().top > 3900){
+            //     console.log('마지막 영역이다', boundingRect.top)
+            //     scrollArea.classList.remove('fixed');
+            // }  
         }
     });
     activate();
+
+    // if(document.currentScript === undefined){
+    //     // IE 에서만 돌아갈 내용
+    // }
 });        
 
 
