759ee68fdb9809a4e6d7a49930aa755dda50f0a8
노보람 <boramm@mindslab.ai>
2021-08-13 19:34:53 +0900

    [추가] 리뉴얼 빌더 - 프로젝트 등록 step 별 페이지

--- maumData_renewal/assets_renewal/js/dashboard.js
+++ maumData_renewal/assets_renewal/js/dashboard.js
@@ -1,5 +1,23 @@
 /* MINDsLab. NBR. 20210716 */
 $(document).ready(function(){
+    // -------------------- 공통 -------------------- //
+    // 공통 layer popup 
+	$('.btn_lyr_open').on('click',function(){	
+        var btnLyrName = $(this).data('lyr-name'),
+            popId = '#' + btnLyrName;
+        
+        // open
+        $(popId).css('display', 'block');
+        $(popId).prepend('<div class="lyr_bg"></div>');
+        
+        // close 
+        $('.btn_lyr_close, .btn_confirm').on('click',function(){
+            $(popId).css('display', 'none'); 
+            $('.lyr_bg').remove(); 
+        });	
+    });	
+
+    
     // snb 열기 / 닫기
     $('#snb').on('click', function(e){
         e.stopPropagation();
