d98c5b919ca5494fe359d1a44daddfcb1a39546c
노보람 <boramm316@gmail.com>
2021-09-24 13:48:12 +0900

    [수정] maum.ai - 메인페이지 js 수정

--- maumAi_main/aiaas/kr/js/text_animation.js
+++ maumAi_main/aiaas/kr/js/text_animation.js
@@ -1,6 +1,7 @@
 // MINDsLab UX/UI. NBR. 20210915
 
-var aiccVisible = false,
+var visualVisible = false,
+    aiccVisible = false,
     smartxVisible = false,
     minutesVisible = false,
     cloudApiVisible = false,
@@ -66,14 +67,22 @@ function sectionFigureAni(){
     // 스크롤 방향 감지
     var documentY = document.documentElement.scrollTop;
     var direction = documentY - window.scrollPosition >= 0 ? 1 : -1;
-    console.log(direction);
 
     window.scrollPosition = documentY; // Update scrollY
+
+    // visual 영역
+    // if(checkVisible2($('.stn.visual')) && !visualVisible){	
+    //     console.log('visual');
+
+    //     $('.stn').removeClass('on');
+    //     visualVisible = true;         
+    // }
 
     // AICC 영역
     if(checkVisible($('.stn.aicc')) && !aiccVisible && direction == 1){	
         console.log('aicc');
 
+        $('.stn').removeClass('on');
         $('.stn.aicc').addClass('on');	        
         new numberCounter('.aicc_fgr01', 768000);
         new numberCounter('.aicc_fgr02', 3600000);
@@ -81,11 +90,14 @@ function sectionFigureAni(){
         new numberCounter('.aicc_fgr04', 1000, '1천만');
         new numberCounter('.aicc_fgr05', 2300000); 
         aiccVisible = true;         
-    }else if(!checkVisible2($('.stn.aicc')) && direction == 1){
+    }else if(!checkVisible2($('.stn.aicc')) && direction == -1){
         $('.stn.aicc').removeClass('on');
         aiccVisible = false;
     }
     if(checkVisible2($('.stn.aicc')) && !aiccVisible && direction == -1){
+        console.log('aicc');
+
+        $('.stn').removeClass('on');
         $('.stn.aicc').addClass('on');	        
         new numberCounter('.aicc_fgr01', 768000);
         new numberCounter('.aicc_fgr02', 3600000);
@@ -102,17 +114,21 @@ function sectionFigureAni(){
     if(checkVisible($('.stn.smartX')) && !smartxVisible && direction == 1){
         console.log('smartX');
 
+        $('.stn').removeClass('on');
         $('.stn.smartX').addClass('on');	 
         new numberCounter('.smartX_fgr01', 5000, '수천개');		
         new numberCounter('.smartX_fgr02', 10000, '백만대');		
         new numberCounter('.smartX_fgr03', 20000, '2만여개');		
         new numberCounter('.smartX_fgr04', 2000);		       
         smartxVisible = true;        
-    }else if(!checkVisible2($('.stn.smartX')) && direction == 1){
+    }else if(!checkVisible2($('.stn.smartX')) && direction == -1){
         $('.stn.smartX').removeClass('on');
         smartxVisible = false;
     }
     if(checkVisible2($('.stn.smartX')) && !smartxVisible && direction == -1){
+        console.log('smartX');
+
+        $('.stn').removeClass('on');
         $('.stn.smartX').addClass('on');	 
         new numberCounter('.smartX_fgr01', 5000, '수천개');		
         new numberCounter('.smartX_fgr02', 10000, '백만대');		
@@ -128,16 +144,20 @@ function sectionFigureAni(){
     if(checkVisible($('.stn.minutes')) && !minutesVisible && direction == 1){
         console.log('minutes');
 
+        $('.stn').removeClass('on');
         $('.stn.minutes').addClass('on');	 
         new numberCounter('.minutes_fgr01', 400000);
         new numberCounter('.minutes_fgr02', 276000);
         new numberCounter('.minutes_fgr03', 29000);        
         minutesVisible = true;       
-    }else if(!checkVisible2($('.stn.minutes')) && direction == 1){
+    }else if(!checkVisible2($('.stn.minutes')) && direction == -1){
         $('.stn.minutes').removeClass('on');
         minutesVisible = false;
     }
     if(checkVisible2($('.stn.minutes')) && !minutesVisible && direction == -1){
+        console.log('minutes');
+
+        $('.stn').removeClass('on');
         $('.stn.minutes').addClass('on');	 
         new numberCounter('.minutes_fgr01', 400000);
         new numberCounter('.minutes_fgr02', 276000);
@@ -152,6 +172,7 @@ function sectionFigureAni(){
     if(checkVisible($('.stn.cloudApi')) && !cloudApiVisible && direction == 1){
         console.log('cloudApi');
 
+        $('.stn').removeClass('on');
         $('.stn.cloudApi').addClass('on');	 
         new numberCounter('.cloudApi_fgr01', 10000);
         new numberCounter('.cloudApi_fgr02', 100000, '수십만');
@@ -159,11 +180,14 @@ function sectionFigureAni(){
         new numberCounter('.cloudApi_fgr04', 3600000);
         new numberCounter('.cloudApi_fgr05', 1000);       
         cloudApiVisible = true;        
-    }else if(!checkVisible2($('.stn.cloudApi')) && direction == 1){
+    }else if(!checkVisible2($('.stn.cloudApi')) && direction == -1){
         $('.stn.cloudApi').removeClass('on');
         cloudApiVisible = false;
     }
     if(checkVisible2($('.stn.cloudApi')) && !cloudApiVisible && direction == -1){
+        console.log('cloudApi');
+
+        $('.stn').removeClass('on');
         $('.stn.cloudApi').addClass('on');	 
         new numberCounter('.cloudApi_fgr01', 10000);
         new numberCounter('.cloudApi_fgr02', 100000, '수십만');
@@ -180,16 +204,20 @@ function sectionFigureAni(){
     if(checkVisible($('.stn.maumData')) && !maumDataVisible && direction == 1){
         console.log('maumData');
 
+        $('.stn').removeClass('on');
         $('.stn.maumData').addClass('on');	 
         new numberCounter('.maumData_fgr01', 4700000);
         new numberCounter('.maumData_fgr02', 1787);
         new numberCounter('.maumData_fgr03', 1356);        
         maumDataVisible = true;
-    }else if(!checkVisible2($('.stn.maumData')) && direction == 1){
+    }else if(!checkVisible2($('.stn.maumData')) && direction == -1){
         $('.stn.maumData').removeClass('on');
         maumDataVisible = false;
     }
     if(checkVisible2($('.stn.maumData')) && !maumDataVisible && direction == -1){
+        console.log('maumData');
+
+        $('.stn').removeClass('on');
         $('.stn.maumData').addClass('on');	 
         new numberCounter('.maumData_fgr01', 4700000);
         new numberCounter('.maumData_fgr02', 1787);
@@ -208,10 +236,10 @@ function checkVisible(elm, eval) {
     var viewportHalfHeight = $(window).height() / 2,
         scrolltop = $(window).scrollTop(),
         elementHalfHeight = $(elm).outerHeight() / 2,
-        // offsetY = $(elm).offset().top + (elementHalfHeight / 2);
-        offsetY = $(elm).offset().top;
+        offsetY = $(elm).offset().top + (elementHalfHeight / 2);
+        // offsetY = $(elm).offset().top;
 
-        console.log(viewportHalfHeight, offsetY)
+        // console.log(viewportHalfHeight, offsetY)
     
     // if(eval == 'object visible') return ((offsetY < (viewportHalfHeight + scrolltop)) && (offsetY > (scrolltop - elementHalfHeight)));
     if(eval == 'object visible') return ((offsetY < (viewportHalfHeight + scrolltop)) && (offsetY > (scrolltop - elementHalfHeight)));
