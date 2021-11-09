d5d236c0f14897d2f4364dda5d3ec50935f71466
노보람 <boramm316@gmail.com>
2021-11-09 17:59:22 +0900

    modify daekyo

--- daekyoKids/resources/js/daekyoKids.js
+++ daekyoKids/resources/js/daekyoKids.js
@@ -1,40 +1,6 @@
 /* MINDsLab. UX/UI Team. NBR. 20211104 */
 
 $(document).ready(function(){
-    // visual 영역 swiper
-    var visualSwiper = new Swiper('.visual.swiper-container', {
-        slidesPerGroup: 1,
-        slidesPerView: 1,
-        loop: true, 
-        observer: true,
-		observeParents: true,
-        touchRatio: 0,
-		pagination: {
-			el: '.swiper-pagination',
-            type: 'fraction',
-            formatFractionCurrent: function(number){
-                return '0' + number;
-            },
-            formatFractionTotal: function(number){
-                return '0' + number;
-            }
-		},
-        navigation: {
-            prevEl: '.swiper-button-prev',
-            nextEl: '.swiper-button-next'
-        },
-        autoplay: {
-            delay: 4000,
-            disableOnInteraction: false
-        },
-        breakpoints: {
-			768: {
-				touchRatio: 1
-			}
-		}
-	});
-
-
     // page top 이동 버튼 노출
     function topBtnShow(){
         var windowWidth = $(window).width(),
@@ -57,7 +23,6 @@ $(document).ready(function(){
     $(window).scroll(function(){
 		$('#header').css('left', 0 - $(this).scrollLeft());
         topBtnShow();
-        sectionFigureAni();
 	});
 
 
@@ -114,54 +79,4 @@ $(document).ready(function(){
             clicked = false;
         }
     });
-
-
-    // 각 영역 텍스트 등장 애니메이션
-    var daekyoKidsVisible = false,
-        sangsangAppVisible = false,
-        steamBoxVisible = false,
-        engHomeBoxVisible = false;
-
-    function sectionFigureAni(){
-        // 대교 상상 Kids 영역
-        if(checkVisible($('.stn:nth-child(2)')) && !daekyoKidsVisible){
-            $('.stn').removeClass('on');   
-            $('.stn:nth-child(2)').addClass('on');         
-            aiccVisible = true;
-        }
-
-        // 상상키즈 APP 영역
-        if(checkVisible($('.stn:nth-child(3)')) && !sangsangAppVisible){
-            $('.stn').removeClass('on');     
-            $('.stn:nth-child(3)').addClass('on');         
-            smartxVisible = true;
-        }
-        
-        // 스팀박스 영역
-        if(checkVisible($('.stn:nth-child(4)')) && !steamBoxVisible){ 
-            $('.stn').removeClass('on');  
-            $('.stn:nth-child(4)').addClass('on');               
-            minutesVisible = true;
-        }
-
-        // 영어홈박스 영역
-        if(checkVisible($('.stn:nth-child(5)')) && !engHomeBoxVisible){     
-            $('.stn').removeClass('on'); 
-            $('.stn:nth-child(5)').addClass('on');             
-            cloudApiVisible = true;
-        }
-    }
-    sectionFigureAni();
-
-    // 스크롤 영역 감지
-    function checkVisible(elm, eval) {
-        eval = eval || 'object visible';
-        var viewportHalfHeight = $(window).height() / 2,
-            scrolltop = $(window).scrollTop(),
-            elementHalfHeight = $(elm).outerHeight() / 2,
-            y = $(elm).offset().top;
-        
-        if(eval == 'object visible') return ((y < (viewportHalfHeight + scrolltop)) && (y > (scrolltop - elementHalfHeight)));
-        if(eval == 'above') return ((y < (viewportHalfHeight + scrolltop)));
-    }
 });
