d456f05bb711273271f1d51f49674b1ecd3cda5c
노보람 <boramm316@gmail.com>
2021-12-09 19:31:47 +0900

    no message

--- maumIFD/resources/js/maumIFD_common.js
+++ maumIFD/resources/js/maumIFD_common.js
@@ -22,12 +22,10 @@ $(document).ready(function(){
         e.stopPropagation();
         $('#snb').toggleClass('open');
     });
-    
-    // snb 외 영역 클릭 시에도 닫기
-    $('html').on('click', function(e){
-        e.stopPropagation();
-        if($(e.target).is('#snb')){
-            $('#snb').removeClass('open');
+
+    $('#snb').on('click', function(){
+        if(!$('#snb').is('.open')){
+            $('#snb').addClass('open');
         }
     });
 
@@ -36,6 +34,14 @@ $(document).ready(function(){
         if($(this).siblings('.sub').length > 0){
             $(this).parent('li').toggleClass('active');
         }
+    });
+
+    $('#snb .nav li a').on('mouseenter', function(){
+        $(this).parents('.third').siblings('a').addClass('hover');
+        $(this).addClass('hover');
+    });
+    $('#snb .nav li a').on('mouseleave', function(){
+        $('#snb .nav li a').removeClass('hover');
     });
 
     // user 메뉴 클릭 시 하위 메뉴 보여주기
@@ -52,5 +58,36 @@ $(document).ready(function(){
         }else{
             $(this).parent().removeClass('active');
         }
+    });
+
+    // table checkbox 개별 / 전체 선택
+    $('.allChk').on('click', function(){
+        var thisTableBox = $(this).parents('.icld_chk'),
+            allCheck = $(this).prop('checked'); 
+        
+        if(allCheck){
+            thisTableBox.find('.eachChk').prop('checked', true); 
+        }else{
+            thisTableBox.find('.eachChk').prop('checked', false); 
+        }
+    });
+    
+    // 모든 체크박스를 클릭하면 버튼 활성화시키기
+    $('.eachChk').on('click', function(e){
+        e.stopPropagation();
+
+        var thisTableBox = $(this).parents('.icld_chk'),
+            eachCheck = $(this).prop('checked'); 
+            
+        // 자식 체크 전체 체크시, 부모 체크박스 체크 됨
+        var checkBoxLength = thisTableBox.find('.eachChk').length,
+            checkedLength = thisTableBox.find('.eachChk:checked').length;
+               
+        // 체크박스가 모두 선택되었을 때 상위 체크박스 선택되도록 설정
+        if(checkBoxLength == checkedLength){
+            thisTableBox.find('.allChk').prop('checked', true); 
+        }else{
+            thisTableBox.find('.allChk').prop('checked', false);
+        }       
     });
 });
