15cff9ccddd649444963dddd5821fdf3d4f9a9a2
boramm <boramm@mindslab.ai>
2022-01-17 16:16:40 +0900

    ZOOM Clone coding

--- zoom/src/server.js
+++ zoom/src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
@@ -9,4 +11,23 @@ app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log("Listening on http://localhost:3000");
-app.listen(3000, handleListen);
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({server});
+
+function onSocketClose(){
+    console.log("Disconnected from the Browser")
+}
+
+const sockets = [];
+
+wss.on("connection", (socket) => {
+    sockets.push(socket);
+    console.log("Connected to Browser");
+    socket.on("close", onSocketClose);
+    socket.on("message", (message) => {
+        sockets.forEach(aSocket => aSocket.send(message.toString()));
+    });
+});
+
+server.listen(3000, handleListen);
